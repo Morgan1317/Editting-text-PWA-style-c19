7169e13cec48e4b0b3fb69ff3f9e7544a9da2378
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-10-02 17:51:17 -0500

    fixed deprecated packages

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,11 +12,11 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//Added TODO logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: id, jate: content });
   const result = await request;
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
 }
   
 
-// TODO: Add logic for a method that gets all the content from the database
+// Added logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
